055a65769693af7d9d64fe096a696ae7a5a5ace6
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-05-05 11:14:57 -0300

    refactor

---
+++ inject.js
@@ -0,0 +1,19 @@
+!(() => {
+  /* @import js/focus-within-polyfill.js */
+})()
+
+!(() => {
+  /* @import js/theme-watcher.js */
+})()
+
+!(() => {
+  /* @import js/minimal-ui.js */
+})()
+
+!(() => {
+  /* @import js/session-restore.js */
+})()
+
+!(() => {
+  /* @import js/project-search-blur.js */
+})()
