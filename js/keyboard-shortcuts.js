fe7bb3fd5e9f0ec02dae15915344258a1a67e04b
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-05-07 02:47:31 -0300

    avoid conflicts with editor shortcuts

--- js/keyboard-shortcuts.js
+++ js/keyboard-shortcuts.js
@@ -1,10 +1,20 @@
+const isInside = (element, className) => (
+  !!element.parentElement && (
+    element.classList.contains(className) ||
+    isInside(element.parentElement, className)
+  )
+)
+
 document.addEventListener('keydown', function(e) {
   const key = e.key.toLowerCase()
   const shift = !!e.shiftKey
   if (!e.metaKey) return
   if (key === 'n') return jQuery('.sidebar .new-file').click()
-  if (key === '?') return application.keyboardShortcutsOverlayVisible.toggle()
-  if (key === '/') return application.keyboardShortcutsOverlayVisible.toggle()
   if (key === ',') return application.userOptionsPopVisible.toggle()
   if (key === 'b' && shift) return application.sidebarIsCollapsed.toggle()
+
+  // avoid conflicts with editor keystrokes
+  if(isInside(e.target, 'text-editor')) return
+  if (key === '/') return application.keyboardShortcutsOverlayVisible.toggle()
+  if (key === '?') return application.keyboardShortcutsOverlayVisible.toggle()
 })
